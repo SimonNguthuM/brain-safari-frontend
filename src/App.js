6ba8d07306628688ce44e87d33f7ea429c0e6682
toel-kamanda34 <toelokemwa@gmail.com>
2024-11-11 13:10:47 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Home from './components/Home'; 
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <h1>Welcome</h1>
-      <Home />  
+      <h1>Brain Safari </h1>
+      <Home />
     </div>
   );
 }
