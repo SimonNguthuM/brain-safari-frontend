d82ad1c9e9d61871540e72b957506fbfcef59001
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-11-15 17:20:47 +0300

    style

--- src/components/Login.js
+++ src/components/Login.js
@@ -46,11 +46,10 @@ const Login = () => {
   });
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-teal-300 to-coral-300 flex justify-center items-center">
-      <form onSubmit={formik.handleSubmit} className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold text-teal-700 mb-4">Login</h2>
-        <div className="input-group mb-4">
-          <label htmlFor="email" className="block text-teal-700">Email:</label>
+    <div className="login-form">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
@@ -58,14 +57,13 @@ const Login = () => {
             value={formik.values.email}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
           />
           {formik.touched.email && formik.errors.email ? (
-            <p className="text-red-500 text-sm">{formik.errors.email}</p>
+            <p style={{ color: "red" }}>{formik.errors.email}</p>
           ) : null}
         </div>
-        <div className="input-group mb-6">
-          <label htmlFor="password" className="block text-teal-700">Password:</label>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
@@ -73,16 +71,13 @@ const Login = () => {
             value={formik.values.password}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
           />
           {formik.touched.password && formik.errors.password ? (
-            <p className="text-red-500 text-sm">{formik.errors.password}</p>
+            <p style={{ color: "red" }}>{formik.errors.password}</p>
           ) : null}
         </div>
-        <button type="submit"  className="w-full py-3 bg-green-500 text-white rounded-full hover:bg-green-600 transition" >
-          Login
-        </button>
-        {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+        <button className="login-btn" type="submit">Login</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
     </div>
   );
