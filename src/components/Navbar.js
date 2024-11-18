20646b09aae7ae23fbddedd0589679ba716494fb
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-11-18 10:29:17 +0300

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useUser } from "../App";
 
 function Navbar({ handleLogout }) {
   const { user } = useUser(); // Use user context to get the current user
-  const location = useLocation(); // Get the current URL path
+
 
   // Get the name of the current route (use last part of pathname as page name)
-  const currentPage = location.pathname.split('/').pop().replace(/_/g, ' '); // Replace underscores with spaces for better readability
 
   return (
     <nav className="navbar fixed top-0 w-full bg-white bg-opacity-90 shadow-md p-4 flex justify-between items-center z-50">
